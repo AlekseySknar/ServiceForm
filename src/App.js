8b4bff739f27706b4a8bfd1ceaeb64a184b03bb9
AlekseySknar <59883118+AlekseySknar@users.noreply.github.com>
2020-01-21 12:03:49 +0700

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,70 +1,79 @@
-import React from 'react';
-import './App.css';
-import { makeStyles } from '@material-ui/core/styles';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
 
-import PhoneAndroidRoundedIcon from '@material-ui/icons/PhoneAndroidRounded';
+import PhoneAndroidRoundedIcon from "@material-ui/icons/PhoneAndroidRounded";
 
 const useStyles = makeStyles({
-    paperHead: {
-        backgroundColor: "#33b5e5",
-        height: 68,
-        padding: "5 0",
-    },
-    media: {
-        height: 140,
-        backgroundColor: "#aaa"
-    },
+  paperHead: {
+    backgroundColor: "#33b5e5",
+    height: 68,
+    padding: "5 0"
+  },
+  media: {
+    height: 140,
+    backgroundColor: "#aaa"
+  },
+  headerText: {
+    color: "white",
+    paddingTop: 12
+  }
 });
 
 function App() {
-
-    const myStyles = useStyles();
+  const myStyles = useStyles();
 
   return (
     <div>
-        <Container maxWidth="md">
+      <Container maxWidth="md">
         <Paper>
+          <div className={myStyles.paperHead}>
+            <Typography
+              variant="h4"
+              align="center"
+              className={myStyles.headerText}
+            >
+              Поиск сервиса
+            </Typography>
+          </div>
 
-            <div className={myStyles.paperHead}>
-                <p>Hello</p>
-            </div>
-
-            <Grid container spacing={3}>
-                <Grid item xs={12} sm={1}>
-                    <PhoneAndroidRoundedIcon fontSize="large" />
-                </Grid>
-                <Grid item xs={12} sm={5}>
-                    <TextField
-                        required
-                        id="firstName"
-                        name="firstName"
-                        label="Phone manufacturer"
-                        fullWidth
-                        autoComplete="fname"
-                    />
-                </Grid>
-
-                <Grid item xs={12} sm={6}>
-                    <TextField
-                        required
-                        id="lastName"
-                        name="lastName"
-                        label="Phone model"
-                        fullWidth
-                        autoComplete="lname"
-                    />
-                </Grid>
-
-                <Grid item xs={12}>
-                    <h1>HH</h1>
-                </Grid>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={1} alignContent="space-between">
+              {/*    <PhoneAndroidRoundedIcon fontSize="large" /> */}
+              <a>sss</a>
             </Grid>
+            <Grid item xs={12} sm={5}>
+              <TextField
+                required
+                id="firstName"
+                name="firstName"
+                label="Phone manufacturer"
+                fullWidth
+                autoComplete="fname"
+              />
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <TextField
+                required
+                id="lastName"
+                name="lastName"
+                label="Phone model"
+                fullWidth
+                autoComplete="lname"
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <h1>HH</h1>
+            </Grid>
+          </Grid>
         </Paper>
-        </Container>
+      </Container>
     </div>
   );
 }
